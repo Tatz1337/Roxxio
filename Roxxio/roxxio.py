da02bb04049f925fff4c07f649be66cfdfc03f1c
Tatz <draccagno@gmail.com>
2021-12-23 17:15:08 -0500

    Add files via upload

---
+++ Roxxio/roxxio.py
@@ -0,0 +1,30 @@
+from colorama import Fore, Back, Style
+print("Imported Colorama!")
+import time
+print("Imported Time!")
+import os 
+print("Imported OS!")
+import numpy
+print("Imported NumPY!")
+import requests
+print("Imported Requests!")
+import pandas
+print("Imported Pandas!")
+import tensorflow
+print("Imported TensorFlow!")
+import scrapy
+print("Imported Scrapy!")
+import keras
+print("Imported Keras!")
+import flask
+print("Imported Flask!")
+import imageai
+print("Imported ImageAI!")
+with open('roxlogo.txt', 'r') as f:
+    for line in f:
+        print(line.rstrip())
+
+print("Standing by for your command!")
+time.sleep(5)
+
+comd = input("What can I do for you? - ")
