af45bd785065a58b5065a8489a8aff3ff2194b2c
Tatz <draccagno@gmail.com>
2021-12-23 18:28:21 -0500

    Add files via upload

--- Roxxio/roxxio.py
+++ Roxxio/roxxio.py
@@ -1,5 +1,7 @@
-from colorama import Fore, Back, Style
-print("Imported Colorama!")
+#imports
+from colorama import Fore, Back, Style, init
+init(convert=True)
+print(Fore.GREEN + "Imported Colorama!")
 import time
 print("Imported Time!")
 import os 
@@ -22,19 +24,38 @@ import imageai
 print("Imported ImageAI!")
 import ctypes
 print("Imported CTypes!")
+# SETS TERMINAL TITLE TEXT
 ctypes.windll.kernel32.SetConsoleTitleW("Roxx-IO")
+import gtts
+print("Imported gtts!")
+from playsound import playsound
+print("Imported Playsound!")
+# END OF IMPORTS
+
+# AI NAME AND LOGO PRINTING
 name = "Roxx-IO"
-
-
-
+print(Style.RESET_ALL)
 with open('roxlogo.txt', 'r') as f:
     for line in f:
         print(line.rstrip())
+# END OF LOGO PRINTING
 
-print("Standing by for your command!")
-time.sleep(5)
+# CHANGE FROM GREEN IMPORTING TEXT COLOR TO NORMAL WHITE
+print(Style.RESET_ALL)
 
-comd = input("What can I do for you? - ")
+# CHECK IF ONLINE
+url = "http://www.kite.com"
+timeout = 5
+request = requests.get(url, timeout=timeout)
+print("I am " + Fore.GREEN + "online!")
+# END OF ONLINE CHECK
 
+# RESET FROM GREEN TEXT FOR ONLINE STATUS BACK TO WHITE
+print(Style.RESET_ALL)
+
+# INPUT PROMPT
+comd = input("What can I do for you~? - ")
+
+# TEST QUESTION + ANSWER
 if comd in ['wru', "who are you", "who are you?", "who are ya?", "who are ya"]: 
 	print ("I am " + name + "." + "    ^.^")
